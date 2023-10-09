ec0e39753ab24b98a16479ca6557eaee2557ec6d
cooskin <2496666065@qq.com>
2023-10-09 20:17:49 +0800

    更新错误驳货

--- src/onMessage.js
+++ src/onMessage.js
@@ -48,69 +48,74 @@ const roomList = config.room.roomList
 // 消息监听回调
 module.exports = bot => {
   return async function onMessage(msg) {
-    // console.log('消息类型{%s},信息内容{%s}',msg.type(),msg.text());
-    // 判断消息来自自己，直接return
-    if (msg.self()) return
+    try {
+      // console.log('消息类型{%s},信息内容{%s}',msg.type(),msg.text());
+      // 判断消息来自自己，直接return
+      if (msg.self()) return
 
-    // 判断此消息类型是否为文本
-    if (msg.type() == 7) {
-      //获取用户信息
-     let user = await api.getUser(msg.payload.talkerId);
-     //输出user正常返回值
-     console.log('用户信息', typeof user, user);
-     if(user.code==1 && user.msg == '记录成功'){
-      console.log('进入记录他通知');
-      T = m('Cooskin',['系统提示:用户登记成功'])
-      console.log('进入通知',T);
-      msg.say(T)
-    }
-      
-      // 判断消息类型来自群聊
-      if (msg.room()) {
-        // 获取群聊
-        const room = await msg.room()
-
-        // 收到消息，提到自己
-        if (await msg.mentionSelf()) {
-          // 获取提到自己的名字
-          let self = await msg.to()
-          self = "@" + self.name()
-          // 获取消息内容，拿到整个消息文本，去掉 @+名字
-          let sendText = msg.text().replace(self, "")
-          // 请求机器人接口回复
-          let res = await requestRobot(sendText)
-          // 返回消息，并@来自人
-          room.say(res, msg.from())
-          return
-        }
-
-        // 收到消息，没有提到自己  忽略
-      } else {
-       // 关键词检测 help中所有的方法 返回了T文本
-        // 日记功能
-        if (await diary(msg)) return
-
-        // 天气功能
-        if (await weatherMsg(msg)) return
-        // 定时任务
-        if (await sleepTask(msg)) return
-        // 记账本
-        if (await cashbook(msg)) return
-        // 火车票
-        if (await ticket(msg)) return
-        if (await help(msg)) return
-
-        // 回复信息是关键字 “加群”
-        if (await isAddRoom(msg)) return
-        // 回复信息是所管理的群聊名
-        if (await isRoomName(bot, msg)) return
-        //请求机器人聊天接口
-        // let res = await requestRobot(msg.text())
-        //返回聊天接口内容
-        // await msg.say(res)
+      // 判断此消息类型是否为文本
+      if (msg.type() == 7) {
+        //获取用户信息
+      let user = await api.getUser(msg.payload.talkerId);
+      //输出user正常返回值
+      console.log('用户信息', typeof user, user);
+      if(user.code==1 && user.msg == '记录成功'){
+        console.log('进入记录他通知');
+        T = m('Cooskin',['系统提示:用户登记成功'])
+        console.log('进入通知',T);
+        msg.say(T)
       }
-    } else {
-      console.log("消息不是文本！")
+        
+        // 判断消息类型来自群聊
+        if (msg.room()) {
+          // 获取群聊
+          const room = await msg.room()
+
+          // 收到消息，提到自己
+          if (await msg.mentionSelf()) {
+            // 获取提到自己的名字
+            let self = await msg.to()
+            self = "@" + self.name()
+            // 获取消息内容，拿到整个消息文本，去掉 @+名字
+            let sendText = msg.text().replace(self, "")
+            // 请求机器人接口回复
+            let res = await requestRobot(sendText)
+            // 返回消息，并@来自人
+            room.say(res, msg.from())
+            return
+          }
+
+          // 收到消息，没有提到自己  忽略
+        } else {
+        // 关键词检测 help中所有的方法 返回了T文本
+          // 日记功能
+          if (await diary(msg)) return
+
+          // 天气功能
+          if (await weatherMsg(msg)) return
+          // 定时任务
+          if (await sleepTask(msg)) return
+          // 记账本
+          if (await cashbook(msg)) return
+          // 火车票
+          if (await ticket(msg)) return
+          if (await help(msg)) return
+
+          // 回复信息是关键字 “加群”
+          if (await isAddRoom(msg)) return
+          // 回复信息是所管理的群聊名
+          if (await isRoomName(bot, msg)) return
+          //请求机器人聊天接口
+          // let res = await requestRobot(msg.text())
+          //返回聊天接口内容
+          // await msg.say(res)
+        }
+      } else {
+        console.log("消息不是文本！")
+      }
+    } catch (e) {
+      T= m('Cooskin',['系统提示:出错了','错误内容：'+e.message])
+      msg.say(T)
     }
   }
 }
